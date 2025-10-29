0a64f886849809c91476151b4a083bb76aa49392
Roiqk7 <sevecekpeta@gmail.com>
2025-10-29 21:38:50 +0100

    feat: Implemented basic workflow
    Now the code works from start to finish

--- src/solverHandler.py
+++ src/solverHandler.py
@@ -1,22 +1,38 @@
 from encoder import ProblemInstance
-from globals import DEFAULT_SOLVER_OUTPUT_FILE_PATH
+from globals import *
 import subprocess
 
-class SolverOutput:
-	pass
-
-class Solution:
-	def __init__(self, solverOutput: SolverOutput):
-		pass
-
 class SolverHandler:
-	def __init_(self, pi: ProblemInstance):
-		self.InputFile = pi.OutputFile
-		# TODO: Make configurable output file
-		self.OutputFile = DEFAULT_SOLVER_OUTPUT_FILE_PATH
+	def __init__(self, pi: ProblemInstance, verbosity: int = 1):
+		logger.debug("Handing the problem instance to the solver...")
+		self._Init(pi, verbosity)
+		rawResult = self.CallSolver()
+		self.WriteResult(rawResult)
+		logger.debug("Finished handling the problem instance")
 
+	def _Init(self, pi: ProblemInstance, verbosity: int):
+		self.InputFile = pi.OutputFile
+		# TODO: Make configurable output file + Verbosity
+		self.OutputFile = DEFAULT_SOLVER_OUTPUT_FILE_PATH
+		# TODO: Better handling but its good enough for now
+		if verbosity in [0, 1, 2]:
+			self.Verbosity = verbosity
+		else:
+			raise ValueError(f"Invalid verbosity level: {verbosity}")
 
 	def CallSolver(self):
-		# TODO: Implement
-		# return subprocess.run(['./' + solver_name, '-model', '-verb=' + str(verbosity), output_name], stdout=subprocess.PIPE)
-		pass
+		with open(self.InputFile) as f:
+			command = [
+				DEFAULT_SOLVER_EXECUTABLE_PATH,
+				"-model",
+				"-verb=" + str(self.Verbosity)
+			]
+			logger.debug("Executing SAT solver command: %s", " ".join(command))
+			return subprocess.run(command, stdin=f, stdout=subprocess.PIPE)
+
+	def WriteResult(self, result):
+		logger.debug(f"Writing SAT solver result to {self.OutputFile}...")
+		data = result.stdout.decode("utf-8")
+		with open(self.OutputFile, "w") as f:
+			f.write(data)
+
