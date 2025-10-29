1f316375e3fb02a2b72c262940f9c2dee85e9844
Roiqk7 <sevecekpeta@gmail.com>
2025-10-29 21:38:07 +0100

    feat: Added logger

--- src/globals.py
+++ src/globals.py
@@ -1,4 +1,14 @@
+import coloredlogs
+import logging
 from pathlib import Path
 
 DEFAULT_CNF_FILE_PATH = Path("data", "in", "input.cnf").as_posix()
 DEFAULT_SOLVER_OUTPUT_FILE_PATH = Path("data", "out", "result.txt").as_posix()
+DEFAULT_SOLVER_EXECUTABLE_PATH = Path("glucose", "simp", "glucose").as_posix()
+
+logger = logging.getLogger("SGP")
+coloredlogs.install(
+	level="DEBUG",
+	fmt="[%(levelname)s][%(name)s][%(asctime)s]: %(message)s",
+	datefmt="%H:%M:%S.%f",
+	logger=logger)
