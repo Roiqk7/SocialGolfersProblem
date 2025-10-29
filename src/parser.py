f9cea908ea6c4c915561d9b4a92de0b7008cdef6
Roiqk7 <sevecekpeta@gmail.com>
2025-10-29 14:12:24 +0100

    feat: Added configurable input/output files

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
 import argparse
+from globals import DEFAULT_CNF_FILE_PATH, DEFAULT_SOLVER_OUTPUT_FILE_PATH
 
 def ParseArgs():
 	parser = argparse.ArgumentParser(description="Social Golfers SAT Solver")
@@ -7,6 +8,8 @@ def ParseArgs():
 	parser.add_argument("--S", type=int, default=4, help="Size of each group (default: 4)")
 	parser.add_argument("--R", type=int, default=10, help="Number of rounds (default: 10)")
 	parser.add_argument("--T", type=int, default=1, help="Max times a pair can meet (default: 1)")
+	parser.add_argument("--I", type=str, default=DEFAULT_CNF_FILE_PATH, help=f"Input file in DIMACS CNF format (default: {DEFAULT_CNF_FILE_PATH}")
+	parser.add_argument("--O", type=str, default=DEFAULT_SOLVER_OUTPUT_FILE_PATH, help=f"Output file of the SAT solver (default: {DEFAULT_SOLVER_OUTPUT_FILE_PATH}")
 	args = parser.parse_args()
 	CheckArgs(args)
 	return args
@@ -23,3 +26,6 @@ def CheckArgs(args):
 	for name, val in [("N", args.N), ("G", args.G), ("S", args.S), ("R", args.R), ("T", args.T)]:
 		if val <= 0:
 			raise ValueError(f"Parameter {name} must be a positive integer, got {val}.")
+
+	# Check if files exist
+	# TODO: Implement
