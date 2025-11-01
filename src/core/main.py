889ae051624c82384b7fae2547364b5b14c94fe4
Roiqk7 <sevecekpeta@gmail.com>
2025-11-01 18:30:53 +0100

    feat: Added programme timer

--- src/core/main.py
+++ src/core/main.py
@@ -1,14 +1,18 @@
 from encapsulator import Encapsulator
 from encoder import ProblemInstance
+from globals import logger
 from parser import ParseArgs
 from solverHandler import SolverHandler
+from time import perf_counter as clock
 
 def Main():
+	startTime = clock()
 	args = ParseArgs()
 	N, G, S, R, T = args.N, args.G, args.S, args.R, args.T
 	problemInstance = ProblemInstance(N, G, S, R, T)
 	solver = SolverHandler(problemInstance)
 	Encapsulator(solver.RawResult, N, R, G, S)
+	logger.info(f"Finished in {(clock() - startTime):.3f} seconds")
 
 if __name__ == "__main__":
 	Main()
