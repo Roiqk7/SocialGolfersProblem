2f59a66d01ed1a6b3a65f7dad2aae037ce55b58c
Roiqk7 <sevecekpeta@gmail.com>
2025-11-02 20:26:21 +0100

    feat: Added

---
+++ src/SocialGolfersProblem/webapi/SGPController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SGPController
+{
+        // TODO: Example: https://github.com/Roiqk7/EigenvalueFinder/blob/main/src/EigenvalueFinder.WebAPI/Controllers/EigenvalueFinderController.cs
+}
