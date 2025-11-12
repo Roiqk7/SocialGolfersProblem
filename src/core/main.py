629ce1a39f39fddd0491bcd6a554ec3a929f215e
Roiqk7 <sevecekpeta@gmail.com>
2025-11-12 14:18:38 +0100

    feat: Stdout result + minor tweaks

--- src/core/main.py
+++ src/core/main.py
@@ -2,22 +2,25 @@ from encapsulator import Encapsulator
 from encoder import ProblemInstance
 from globals import *
 from parser import ParseArgs
+from resultWriter import ResultWriter
 from solverHandler import SolverHandler
 from time import perf_counter as clock
 
 def Main():
+	startTime = clock()
 	try:
-		startTime = clock()
 		args = ParseArgs()
 		N, G, S, R, T = args.N, args.G, args.S, args.R, args.T
 		problemInstance = ProblemInstance(N, G, S, R, T)
 		solver = SolverHandler(problemInstance, args.V)
-		Encapsulator(solver.RawResult, N, R, G, S)
+		encapsulator = Encapsulator(solver.RawResult, N, R, G, S)
+		ResultWriter(encapsulator.FormatedSchedule)
 		logger.info(f"Finished in {(clock() - startTime):.3f} seconds")
 		# Unsolvable - return 1 exit code else 0 if solvable
 		return 1 if solver.RawResult.returncode == 20 else 0
 	except Exception as e:
 		logger.error(e)
+		logger.info(f"Finished in {(clock() - startTime):.3f} seconds")
 		# Could not finish the program
 		return 2
 
